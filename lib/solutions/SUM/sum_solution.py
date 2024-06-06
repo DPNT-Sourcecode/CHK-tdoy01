e2ff9714231fbf7273fca9c74f69e005f33ce667
sbx_user1051 <sbx_user1051@169.254.123.133>
2024-06-06 14:45:28 +0000

    Thu Jun 06 14:45:28 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,7 +1,11 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
+def compute(x: int, y: int):
     if x < 0 or y < 0:
         raise Exception(f"x and y can not be less than 0. Given x: '{x}', y: '{y}")
+
+    if x > 100 or y > 100:
+        raise Exception(f"x and y can not be greater than 100. Given x: '{x}', y: '{y}")
     return x + y
+
 
 
