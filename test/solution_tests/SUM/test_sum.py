85fc78c1d92a741d3d1b9397976a1213150f2a8f
sbx_user1051 <sbx_user1051@169.254.123.133>
2024-06-06 14:41:08 +0000

    Thu Jun 06 14:41:08 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,15 @@
+import pytest
+
 from solutions.SUM import sum_solution
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+# test sum with a negative integer returns an error
+    def test_sum_negative_integer_returns_an_error(self):
+        pytest.raises()
+# test sum at the boundary of 0 and 100
+
+
