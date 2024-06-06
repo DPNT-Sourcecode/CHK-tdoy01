6300d73ab02712b3b3eadad6613d7745dffc5ad6
sbx_user1051 <sbx_user1051@169.254.123.133>
2024-06-06 15:08:35 +0000

    Thu Jun 06 15:08:35 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,17 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus: str):
-    return 100
+
+price_table: dict = {"A": 50, "B": 30, "C": 20, "D": 15}
+
+def checkout(skus: str) -> int:
+    skus = skus.split(" ")
+    total = 0
+    for sku in skus:
+        item_price = price_table.get(sku, None)
+        if item_price is None:
+            return -1
+        total += item_price
+    return total
+
 
--- test/solution_tests/CHK/checkout_test.py
+++ test/solution_tests/CHK/checkout_test.py
@@ -5,3 +5,8 @@ class TestCheckout:
     def test_multiple_items_total(self):
         assert checkout("A B C") == 100
         assert checkout("A C") == 70
+        assert checkout("A B C D") == 115
+
+    def test_return_negative_one_when_unable_to_find_item(self):
+        assert checkout("E") == -1
+
