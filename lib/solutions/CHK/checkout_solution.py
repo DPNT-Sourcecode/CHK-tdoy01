d04fb5aa2609a9569dfb4d0402032662f44b2486
sbx_user1051 <sbx_user1051@169.254.123.133>
2024-06-06 15:46:10 +0000

    Thu Jun 06 15:46:10 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,43 +6,50 @@ price_table: dict[str, int] = {"A": 50, "B": 30, "C": 20, "D": 15}
 special_offers: dict[str, tuple] = {"A": (3, 30), "B": (2, 15)}
 
 
-class SpecialOffer(abc.ABCMeta):
-
-    @abc.abstractmethod
-    def apply_offer(self):
-        pass
-
-
-class XForYOffer(SpecialOffer):
-
-    def __init__(self, item_count: int, next_item_price: int):
-        super().__init__()
-        self.item_count = item_count
-        self.next_item_price = next_item_price
-
-    def apply_offer(self):
-        return 0
+# class SpecialOffer(abc.ABC):
+#
+#     @abc.abstractmethod
+#     def apply_offer(self):
+#         pass
+#
+#
+# class XForYOffer(SpecialOffer):
+#
+#     def __init__(self, item_count: int, next_item_price: int):
+#         self.item_count = item_count
+#         self.next_item_price = next_item_price
+#
+#     def check_item_has_offer(self):
+#         if
+#
+#     def apply_offer(self):
+#         return self.next_item_price
 
 
 def checkout(skus: str) -> int:
     skus = skus.replace(" ", "")
     total = 0
     sku_count = {}
+    # count skus
     for sku in skus:
         if sku not in sku_count:
             sku_count.update({sku: 1})
         else:
             sku_count.update({sku: sku_count.get(sku) + 1})
 
+    # apply discounts
+    for sku, count in sku_count.items():
         item_price = price_table.get(sku, None)
         if item_price is None:
             return -1
 
         if sku in special_offers:
-            if sku_count.get(sku) % special_offers.get(sku)[0] == 0:
+            if count % special_offers.get(sku)[0] == 0:
                 item_price = special_offers.get(sku)[1]
 
         total += item_price
+
     return total
+
 
 
